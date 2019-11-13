0f92a6a15beb916425727a29c060cfeb0cec5e4e
Rafik Frangulyan <rafic.frangulyan@betconstruct.com>
2019-11-13 17:25:05 +0400

    build

---
+++ precache-manifest.b03a6ef2c77f552dbe71cb288b51e859.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc60ca3760612a8497046a7315191fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e81c5f9ef4d591ec4b83",
+    "url": "/static/css/main.04a5160c.chunk.css"
+  },
+  {
+    "revision": "4cd594c8b9cb66a2ab6f",
+    "url": "/static/js/2.7273fdce.chunk.js"
+  },
+  {
+    "revision": "e81c5f9ef4d591ec4b83",
+    "url": "/static/js/main.4a31d19a.chunk.js"
+  },
+  {
+    "revision": "73564336732b737fdbc5",
+    "url": "/static/js/runtime-main.ae7b9c74.js"
+  }
+]);
